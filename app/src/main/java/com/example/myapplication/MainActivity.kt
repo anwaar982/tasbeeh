72c8d32be2f8f767be8719190d05000793a89faf
Anwaar Raafat <raafatanwaar@gmail.com>
2023-03-16 13:59:28 +0200

    make tasbeeh app using lottie library

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.myapplication.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
+var counter=0
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+         binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.btnCounter.setOnClickListener {
+
+            counter++
+            binding.textView.text=counter.toString()
+        }
+
+
+
+
+}}
